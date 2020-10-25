e0fce5560add915e556dec7546901aae440b09dc
Hector Lozano <user.email>
2020-10-25 15:44:29 +0000

    Header

--- holberton.h
+++ holberton.h
@@ -2,8 +2,24 @@
 #define HOLBERTON_H_
 
 int _printf(const char *format, ...);
+int _strlen_recursion(char *s);
+int _putchar(char c);
+void strrev(char *s);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_int(va_list list);
+int print_u_int(va_list list);
+int print_binary(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_rev(va_list list);
+int print_rot13(va_list list);
+int(*funtion (char))(va_list list);
 
-
+typedef strcut formats{
+	char *specifier;
+	int(*f)(va_list list);
+}format_specifiers;
 
 
 #endif /* HOLBERTON_H_ */
