ea800d4d6bd8720dae84276e99d1fbc1aea2b88f
Hector Lozano <user.email>
2020-10-25 15:59:35 +0000

    Helper

--- helper_functions.c
+++ helper_functions.c
@@ -16,39 +16,20 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * _atoi - convert a string s to an integer
- * @s: The source array
- *
- * Return: The integer value or 0 if s isn't a number
+ * itoa - convert an interger to a string
+ * @i: ingerger
+ * @strout: Our strout
+ * @base: Base
+ * Return: character from interger
  */
-int _atoi(char *s)
+char *itoa(int i, char *strout, int base)
 {
-	int i, sign = 1, digit = 0, st, en = 0, b = 1;
-	int r = 0;
-
-	for (i = 0; s[i] != '\0' && b; i++)
-	{
-		if (s[i] == '+' && digit != 1)
-			sign = sign * 1;
-		else if (s[i] == '-' && digit != 1)
-			sign = sign * -1;
-		else if (s[i] >= '0' && s[i] <= '9' && digit == 0)
-		{
-			digit = 1;
-			st = i;
-		}
-		else if (digit == 1 && (s[i] < '0' || s[i] > '9'))
-		{
-			b = 0;
-			en = i - 1;
-		}
+	char *str = strout;
+	int digit, sign = 0;
+	if (i < 0) {
+		sign = 1;
+		i *= -1;
 	}
-	if (en == 0)
-		en = i - 1;
-	for (i = st; i <= en; i++)
-		r += (s[i] - 48) * _pow(10, (en - i)) * sign;
-	return (r);
-}
 
 /**
  * _putchar - writes the character c to stdout
@@ -62,7 +43,6 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-#include "holberton.h"
 /**
  * strrev - prints string in reverse
  * @s: takes in a string
