6ceced43b2bdc81d0414a57d5e7e0bea2c7db223
realtalishaw <misstalishawhite@gmail.com>
2020-10-27 19:40:28 +0000

    whitespace accounted for, thanks Carlos

--- get_format_func.c
+++ get_format_func.c
@@ -13,37 +13,30 @@ int get_format_func(const char *format, format_specifiers fmt[], va_list list)
 {
 	int i, j, val, len;
 
-	len = 0;
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
+			while (format[i + 1] == ' ')
+				i++;
 			for (j = 0; fmt[j].specifier != NULL; j++)
 			{
 				if (format[i + 1] == fmt[j].specifier[0])
 				{
-					val = fmt[j].f(list);
-					if (val == -1)
-						return (-1);
-					len = len + val;
-					break;
-				}
-				if (format[i + 2] == fmt[j].specifier[0])
-				{
-					i++;
-					val = fmt[j].f(list);
+				 	val = fmt[j].f(list);
 					if (val == -1)
 						return (-1);
 					len = len + val;
 					break;
 				}
 			}
-			if (fmt[j].specifier == NULL && format[i + 1] != ' ')
+			if (fmt[j].specifier == NULL && format[i + 1] == ' ')
 			{
 				if (format[i + 1] != '\0')
 				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
+			       		_putchar(format[i]);
+
 					len = len + 2;
 				}
 				else
